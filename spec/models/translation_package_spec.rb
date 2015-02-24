2c12e7fc26d3fbeaf0ce0ed76756a2369bcf4029
Ivan Morais <ivan.eng.controle@gmail.com>
2015-02-24 18:20:20 -0300

    Add translation package model.

---
+++ spec/models/translation_package_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe TranslationPackage, type: :model do
+  before do
+    $gengo = Gengo::API.new({
+      :public_key => "Q3YJlTgT3^jI5P_^UJm9Au]6Ws9omzWs]ThxZ8)|bLyhxN[a8$HE5NuTk-JSqQBj",
+      :private_key => "pJXjrtIh4J9tOM3-aXvulFsd4skE3ie_SHjuT(WN|{lHNX6BLkjyI(o7eURK7W[w",
+      :sandbox => true,
+      :api_version => '2'
+    })
+  end
+  
+  describe "#create" do
+    before do
+      @localization = FactoryGirl.create(:localization, locale: "de")
+      @translation_package = TranslationPackage.create([@localization.id])
+    end
+    
+    it "create a proper package" do
+      expect(@translation_package.data).to eql({jobs: {
+                                                    job_0: {
+                                                      :type => "text",
+                                                      :slug => @localization.path,
+                                                      :body_src => @localization.value,
+                                                      :lc_src => "en",
+                                                      :lc_tgt => @localization.locale,
+                                                      :tier => "standard",
+                                                      :auto_approve => "1"
+                                                    }
+                                                  }
+                                                })
+    end
+  end
+  
+  describe "#send_to_gengo" do
+    context "when request is ok"
+      before do
+        @localization = FactoryGirl.create(:localization, locale: "de")
+        @localization2 = FactoryGirl.create(:localization, locale: "de", value: "other value")
+        @translation_package = TranslationPackage.create([@localization.id, @localization2.id])
+        @translation_package.send_to_gengo
+      end
+    
+      it "change job status" do
+        expect(@localization.reload.status).to eql "available"
+      end
+    
+      it "set job id" do
+        expect(@localization.reload.job_id).to_not be_nil
+      end
+    end
+  end
+end
