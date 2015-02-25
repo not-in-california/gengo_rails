a0a56cd7149014666fd4551a169bd4be2f511efa
Ivan Morais <ivan.eng.controle@gmail.com>
2015-02-25 18:52:41 -0300

    Process gengo callback package.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   scope "/gengo" do
     resources :localizations do
       collection do
         post :import
         get :test_import
       end
+=======
+  resources :localizations do
+    collection do
+      post :import
+      get :test_import
+      post :update_job
+>>>>>>> Process gengo callback package.
     end
   end
 end
